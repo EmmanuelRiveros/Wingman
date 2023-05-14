c679da5dec4383bb5ae062a1fc61f91ee58b7e6b
jorge <jorge@DESKTOP-MGBQ8FR>
2023-05-13 21:38:40 -0500

    clase nueva

---
+++ Wingman/src/mx/itson/wingman/entidades/Usuario.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.itson.wingman.entidades;
+
+/**
+ *
+ * @author jorge
+ */
+public class Usuario {
+
+    
+      private int clave;
+    private String nombre;
+    private String contraseña;
+    private String telefono;
+    /**
+     * @return the clave
+     */
+    public int getClave() {
+        return clave;
+    }
+
+    /**
+     * @param clave the clave to set
+     */
+    public void setClave(int clave) {
+        this.clave = clave;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the contraseña
+     */
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    /**
+     * @param contraseña the contraseña to set
+     */
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    /**
+     * @return the telefono
+     */
+    public String getTelefono() {
+        return telefono;
+    }
+
+    /**
+     * @param telefono the telefono to set
+     */
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+  
+}
