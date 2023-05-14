10933103740ba18f022148f9ec66b0edabc2102e
Emmanuel Enrique Riveros Ramírez <Emmanuel-Riveros@outlook.es>
2023-05-13 18:25:58 -0700

    Incluye algunas entidades, enumeradores y DAO

---
+++ Wingman/src/mx/itson/wingman/entidades/Contratista.java
@@ -0,0 +1,56 @@
+package mx.itson.wingman.entidades;
+
+/**
+ *
+ * @author Emmanuel
+ */
+public class Contratista {
+    
+    private String nombre;
+    private String trabajo;
+    private String contacto;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the trabajo
+     */
+    public String getTrabajo() {
+        return trabajo;
+    }
+
+    /**
+     * @param trabajo the trabajo to set
+     */
+    public void setTrabajo(String trabajo) {
+        this.trabajo = trabajo;
+    }
+
+    /**
+     * @return the contacto
+     */
+    public String getContacto() {
+        return contacto;
+    }
+
+    /**
+     * @param contacto the contacto to set
+     */
+    public void setContacto(String contacto) {
+        this.contacto = contacto;
+    }
+    
+    
+}
---
+++ Wingman/src/mx/itson/wingman/enumeradores/TipoEquipamiento.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.itson.wingman.enumeradores;
+
+/**
+ *
+ * @author Emmanuel
+ */
+public enum TipoEquipamiento {
+    MISIL_AIRE_AIRE, MISIL_AIRE_TIERRA,
+    BOMBA_ANTI_TANQUE, BOMBA_ANTI_BUQUE
+}
