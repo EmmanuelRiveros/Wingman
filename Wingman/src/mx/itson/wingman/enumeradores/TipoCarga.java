49b03f126d8efb4402f54b752a3795753e3298cc
genopro <114516286+genopro@users.noreply.github.com>
2023-05-14 21:36:29 -0700

    añadí los enumeradores

---
+++ Wingman/src/mx/itson/wingman/enumeradores/TipoCarga.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.itson.wingman.enumeradores;
+
+/**
+ *
+ * @author paulp
+ */
+
+        public enum TipoCarga {
+            LIGERA,PESADA,PELIGROSA,FRAGIL
+    }
+
