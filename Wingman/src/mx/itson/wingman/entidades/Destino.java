76fb430a7644625cbd378690ad3bd5c068c323c2
genopro <114516286+genopro@users.noreply.github.com>
2023-05-14 21:55:24 -0700

    Add files via upload

---
+++ Wingman/src/mx/itson/wingman/entidades/Destino.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.itson.wingman.entidades;
+
+/**
+ *
+ * @author paulp
+ */
+public class Destino {
+
+    public class destino {
+      private int airbaseld;
+        private String airbaseNombre;
+        private Ruta Ruta;
+        private String ubicacion;
+        /**
+         * @return the airbaseld
+         */
+        public int getAirbaseld() {
+            return airbaseld;
+        }
+
+        /**
+         * @param airbaseld the airbaseld to set
+         */
+        public void setAirbaseld(int airbaseld) {
+            this.airbaseld = airbaseld;
+        }
+
+        /**
+         * @return the airbaseNombre
+         */
+        public String getAirbaseNombre() {
+            return airbaseNombre;
+        }
+
+        /**
+         * @param airbaseNombre the airbaseNombre to set
+         */
+        public void setAirbaseNombre(String airbaseNombre) {
+            this.airbaseNombre = airbaseNombre;
+        }
+
+        /**
+         * @return the Ruta
+         */
+        public Ruta getRuta() {
+            return Ruta;
+        }
+
+        /**
+         * @param Ruta the Ruta to set
+         */
+        public void setRuta(Ruta Ruta) {
+            this.Ruta = Ruta;
+        }
+
+        /**
+         * @return the ubicacion
+         */
+        public String getUbicacion() {
+            return ubicacion;
+        }
+
+        /**
+         * @param ubicacion the ubicacion to set
+         */
+        public void setUbicacion(String ubicacion) {
+            this.ubicacion = ubicacion;
+        }
+  
+        
+    }
+}
---
+++ Wingman/src/mx/itson/wingman/entidades/Ruta.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.wingman.entidades;
+
+/**
+ *
+ * @author paulp
+ */
+public class Ruta {
+    private String nombre;
+    private String zona;
+    private String tipo;
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the zona
+     */
+    public String getZona() {
+        return zona;
+    }
+
+    /**
+     * @param zona the zona to set
+     */
+    public void setZona(String zona) {
+        this.zona = zona;
+    }
+
+    /**
+     * @return the tipo
+     */
+    public String getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    
+}
